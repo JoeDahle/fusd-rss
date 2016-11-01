f105ffa538fddf2b7adca3f451ae8f4c312c96b6
Joe Dahle <joe.dahle@gmail.com>
2016-10-31 17:00:37 -0700

    feat: add quick workaround

--- src/FeedComponent.js
+++ src/FeedComponent.js
@@ -2,15 +2,30 @@ import React from 'react';
 
 import style from './sass/feed.scss';
 
+let location = document.location.protocol + '//fusdaz.parentlink.net';
+let o = 6423;
+
 var FeedComponent = React.createClass({
+  rssCallback: function(){
+      let src = 'https://fusdaz.parentlink.net/static/base/rss/widget/scripts.js';
+      fetch(src, {
+        mode: 'no-cors'
+      })
+          .then(function(response){
+            return response;
+          })
+          .then(function(myblob){
+            console.log(myblob);
+          })
+      // s.parentNode.insertBefore(pl, s)
+  },
+
   render: function(){
     return (
       <div className="feed_component-container">
         <h1 className="feed_component-header">FUSD Emergency Notices</h1>
         <hr></hr>
-        <div className="feed_component-body">
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-        </div>
+        {this.rssCallback()}
       </div>
     );
   }
